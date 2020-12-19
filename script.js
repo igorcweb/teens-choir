bd5d2238df7f19c1dc156d60856344c372769a91
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-18 20:07:27 -0600

    Updates

--- script.js
+++ script.js
@@ -1,209 +1,31 @@
-// $("#palmSunday").collapse("show");
 
-$("a.palm-sunday").on("click", function () {
+$("a.advent-4").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#palmSunday").removeClass("d-none");
+  $("#advent-4").removeClass("d-none");
 });
-
-$("a.holy-thursday").on("click", function () {
+$("a.hf").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#holyThursday").removeClass("d-none");
+  $("#hf").removeClass("d-none");
 });
-
-$("a.good-friday").on("click", function () {
+$("a.ce").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#goodFriday").removeClass("d-none");
+  $("#ce").removeClass("d-none");
 });
-
-$("a.easter-vigil").on("click", function () {
+$("a.mm").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#easterVigil").removeClass("d-none");
+  $("#mm").removeClass("d-none");
 });
-
-$("a.easter-sunday").on("click", function () {
+$("a.cd").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#easterSunday").removeClass("d-none");
-});
-
-$("a.easter-2").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#easter-2").removeClass("d-none");
-});
-
-$("a.easter-3").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#easter-3").removeClass("d-none");
-});
-
-$("a.easter-4").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#easter-4").removeClass("d-none");
-});
-
-$("a.easter-5").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#easter-5").removeClass("d-none");
-});
-
-$("a.easter-6").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#easter-6").removeClass("d-none");
-});
-
-$("a.corpus-christi").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#corpus-christi").removeClass("d-none");
-});
-
-$("a.mothers-day").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#mothers-day").removeClass("d-none");
-});
-
-$("a.ascension").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ascension").removeClass("d-none");
-});
-
-$("a.pentecost").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#pentecost").removeClass("d-none");
-});
-$("a.first-communion").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#first-communion").removeClass("d-none");
-});
-$("a.holy-trinity").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#holy-trinity").removeClass("d-none");
-});
-$("a.ota-12").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-12").removeClass("d-none");
-});
-$("a.ota-13").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-13").removeClass("d-none");
-});
-$("a.ota-14").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-14").removeClass("d-none");
-});
-$("a.ota-15").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-15").removeClass("d-none");
-});
-$("a.ota-16").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-16").removeClass("d-none");
-});
-$("a.ota-17").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-17").removeClass("d-none");
-});
-
-$("a.ota-22").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-22").removeClass("d-none");
-});
-$("a.school").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#school").removeClass("d-none");
-});
-$("a.ota-23").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-23").removeClass("d-none");
-});
-$("a.ota-25").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-25").removeClass("d-none");
-});
-$("a.ota-26").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-26").removeClass("d-none");
-});
-$("a.ota-27").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ota-27").removeClass("d-none");
-});
-$("a.ctk").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ctk").removeClass("d-none");
-});
-$("a.thanksgiving").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#thanksgiving").removeClass("d-none");
-});
-
-$("a.advent-1").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#advent-1").removeClass("d-none");
-});
-$("a.advent-2").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#advent-2").removeClass("d-none");
+  $("#cd").removeClass("d-none");
 });
