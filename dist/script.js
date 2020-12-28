498d00d4f152ec6274923975c151b67fe9e89b4b
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-27 20:49:42 -0600

    mmg

--- dist/script.js
+++ dist/script.js
@@ -1,31 +1,7 @@
 
-$("a.advent-4").on("click", function () {
+$("a.mary").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#advent-4").removeClass("d-none");
-});
-$("a.hf").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#hf").removeClass("d-none");
-});
-$("a.ce").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#ce").removeClass("d-none");
-});
-$("a.mm").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#mm").removeClass("d-none");
-});
-$("a.cd").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#cd").removeClass("d-none");
+  $("#mary").removeClass("d-none");
 });
