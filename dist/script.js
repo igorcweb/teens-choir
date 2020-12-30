3969f64cf040fbbf8ef1b1c07e4177ce3ebe7c5f
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2020-12-29 19:09:25 -0600

    epiphany otb2

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,17 @@ $("a.mary").on("click", function () {
   $(".music").addClass("d-none");
   $("#mary").removeClass("d-none");
 });
+
+$("a.epiphany").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#epiphany").removeClass("d-none");
+});
+$("a.ot2").on("click", function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+  $(".music").addClass("d-none");
+  $("#ot2").removeClass("d-none");
+});
+
