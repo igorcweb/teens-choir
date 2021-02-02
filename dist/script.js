4fa4c449e75939732b5b47db46cd2f08bbc50844
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-02-01 20:29:07 -0600

    otb5

--- dist/script.js
+++ dist/script.js
@@ -4,11 +4,11 @@ $("a.ot3").on("click", function () {
   $(".music").addClass("d-none");
   $("#ot3").removeClass("d-none");
 });
-$("a.ot4").on("click", function () {
+$("a.ot5").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#ot4").removeClass("d-none");
+  $("#ot5").removeClass("d-none");
 });
 
 
