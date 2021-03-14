be09131ce8463e2c0ef793fd6a07188ad5e0e466
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-03-14 18:39:42 -0500

    l5

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,14 @@
-$("a.l3").on("click", function () {
+$("a.lm").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#l3").removeClass("d-none");
+  $("#lm").removeClass("d-none");
 });
-$("a.l4").on("click", function () {
+$("a.l5").on("click", function () {
   $("a").removeClass("active");
   $(this).addClass("active");
   $(".music").addClass("d-none");
-  $("#l4").removeClass("d-none");
+  $("#l5").removeClass("d-none");
 });
 
 
