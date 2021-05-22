98c8ea31f25fbf37ad96f9133f0fc8fe27b60949
Igor Chernyshev <igor.chernyshev@cygrpdal.local>
2021-05-21 20:41:41 -0500

    Updates

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-$("a.mp").on("click", function () {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  $(".music").addClass("d-none");
-  $("#mp").removeClass("d-none");
-});
 
-$("a.pentecost").on("click", function () {
+
+function activate() {
+  $.each($('.selection'), function (i, selection) {
+    if ($(selection).hasClass('active')) {
+      $(selection).find('.fa-hand-pointer-o').addClass('d-none')
+      $(selection).find('.fa-check').removeClass('d-none')
+    }
+  })
+}
+
+activate();
+
+$('.selection').on('click', function () {
   $("a").removeClass("active");
   $(this).addClass("active");
+  $('.selection').find('.fa-hand-pointer-o').removeClass('d-none')
+  $('.selection').find('.fa-check').addClass('d-none')
+  activate();
   $(".music").addClass("d-none");
-  $("#pentecost").removeClass("d-none");
-});
+  if ($(this).hasClass('trinity')) {
+    $("#trinity").removeClass("d-none");
+  } else if ($(this).hasClass('mp')) {
+    $("#mp").removeClass("d-none");
+  }
+  else if ($(this).hasClass('pentecost')) {
+    $("#pentecost").removeClass("d-none");
+  }
+})
+
+$(document).on('mouseover', '.selection', function () {
+  $(this).find('.fa-hand-pointer-o').addClass('hover');
+})
+
+$(document).on('mouseout', '.selection', function () {
+  $(this).find('.fa-hand-pointer-o').removeClass('hover');
+})
+
+
+
+
+
+
 
